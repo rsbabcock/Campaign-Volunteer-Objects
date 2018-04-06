1d0246650ead0ee050d8bee3887f488eb2694c2b
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-06 10:49:40 -0500

    Added detailed comments for assignment

---
+++ main.js
@@ -0,0 +1,35 @@
+// Your job is to define the different objects and resources, 
+// their structure, and the corresponding properties for each, 
+// to represent the following information about Elizabeth's campaign.
+
+// 1. Her congressional district (you can use yours here)
+
+// 2. Her platform statements for the following issues.
+    //  Taxes
+    // Jobs
+    // Infrastructure
+    // Health care
+    // Crime and enforcement
+
+// 3. URL for donation form
+
+// 4. Calendar of events
+
+// 5. Volunteer information
+    // Name
+    // Address
+    // Email
+    // Phone number
+    // Availability
+    // What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+
+// 6. Biography
+
+// 7. Image gallery
+    // Head shot
+    // Picture of family
+    // Picture of constituents
+
+// 8. Mission statement
+
+// 9. URL for registering to vote
